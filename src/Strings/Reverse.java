c2a998024ddd4b509777b304119dd57d4244f2d5
justignatoff <justignatoff@gmail.com>
2019-01-13 21:51:04 +0300

    solution of Java String Reverse

---
+++ src/Strings/Reverse.java
@@ -0,0 +1,19 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        String B = new StringBuilder(A).reverse().toString();
+        if (A.equals(B)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+    }
+}
