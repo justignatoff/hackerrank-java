a73faeaafabd8ebd4144928b0e99bf5c69d11c35
justignatoff <justignatoff@gmail.com>
2019-01-14 22:57:40 +0300

    solution of Java String Anagram

---
+++ src/Strings/Anagrams.java
@@ -0,0 +1,31 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class Anagrams {
+    static boolean isAnagram(String a, String b) {
+        // Complete the function
+        int[] aChars = a.toUpperCase().chars().sorted().toArray();
+        int[] bChars = b.toUpperCase().chars().sorted().toArray();
+        boolean resualt = true;
+        if (aChars.length != bChars.length) {
+            return false;
+        }
+        for (int i = 0; i < aChars.length; i++) {
+            if (aChars[i] != bChars[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
