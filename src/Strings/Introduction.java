1cc93de363e125d10c682dae763b1c886f0183aa
justignatoff <justignatoff@gmail.com>
2019-01-15 15:00:04 +0300

    solution of Java String Introduction

---
+++ src/Strings/Introduction.java
@@ -0,0 +1,23 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class Introduction {
+
+    public static String firstUpperCase(String word){
+        if(word == null || word.isEmpty()) return "";//или return word;
+        return word.substring(0, 1).toUpperCase() + word.substring(1);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+
+        System.out.println(A.length() + B.length());
+        System.out.println(A.compareTo(B) > 0 ? "Yes" : "No");
+        System.out.println(firstUpperCase(A) + " " + firstUpperCase(B));
+
+    }
+}
