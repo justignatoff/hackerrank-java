c0be8d871bc88c2d73bd961b427cb5088a86bc24
justignatoff <justignatoff@gmail.com>
2019-01-26 19:24:05 +0300

    solution of Java HashSet

---
+++ src/Introduction/JavaHashSet.java
@@ -0,0 +1,25 @@
+package Introduction;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class JavaHashSet {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < pair_left.length; i++) {
+            set.add(pair_left[i].concat(" ").concat(pair_right[i]));
+            System.out.println(set.size());
+        }
+
+    }
+}
