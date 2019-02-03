2c50eaf53da06fb5b2998eeb0c52a09899c5c34c
justignatoff <justignatoff@gmail.com>
2019-02-03 19:51:58 +0300

    solution of Java Vararg sum

---
+++ src/Introduction/JavaVarArgsSum.java
@@ -0,0 +1,57 @@
+package Introduction;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringJoiner;
+
+class Add {
+    void add(int... elems){
+        int sum = 0;
+        StringJoiner joiner = new StringJoiner("+");
+        for (int i : elems) {
+            sum += i;
+            joiner.add("" + i);
+        }
+        System.out.println(joiner.toString() + "=" + sum);
+    }
+}
+public class JavaVarArgsSum {
+    public static void main(String[] args) {
+        try{
+            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+            int n1=Integer.parseInt(br.readLine());
+            int n2=Integer.parseInt(br.readLine());
+            int n3=Integer.parseInt(br.readLine());
+            int n4=Integer.parseInt(br.readLine());
+            int n5=Integer.parseInt(br.readLine());
+            int n6=Integer.parseInt(br.readLine());
+            Add ob=new Add();
+            ob.add(n1,n2);
+            ob.add(n1,n2,n3);
+            ob.add(n1,n2,n3,n4,n5);
+            ob.add(n1,n2,n3,n4,n5,n6);
+            Method[] methods=Add.class.getDeclaredMethods();
+            Set<String> set=new HashSet<>();
+            boolean overload=false;
+            for (Method method : methods) {
+                if (set.contains(method.getName())) {
+                    overload = true;
+                    break;
+                }
+                set.add(method.getName());
+
+            }
+            if(overload)
+            {
+                throw new Exception("Overloading not allowed");
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
