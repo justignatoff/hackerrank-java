fe39e0d858d4d9d1ee7ba284f491d8781612c549
justignatoff <justignatoff@gmail.com>
2019-02-19 00:57:57 +0300

    solution of Java MD5

---
+++ src/Introduction/JavaMD5.java
@@ -0,0 +1,25 @@
+package Introduction;
+
+import java.security.MessageDigest;
+import java.util.Scanner;
+
+
+public class JavaMD5 {
+    public static String getMD5Checksum(byte[] checksum) throws Exception {
+        byte[] b = checksum;
+        String result = "";
+
+        for (int i=0; i < b.length; i++) {
+            result += Integer.toString( ( b[i] & 0xff ) + 0x100, 16).substring( 1 );
+        }
+        return result;
+    }
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+        messageDigest.reset();
+        messageDigest.update(s.getBytes());
+        System.out.println(getMD5Checksum(messageDigest.digest()));
+    }
+}
