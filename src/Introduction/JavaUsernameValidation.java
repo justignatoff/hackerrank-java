12dbdd37d5576f9812d9d78c20f7602bda249d28
justignatoff <justignatoff@gmail.com>
2019-02-23 19:13:24 +0300

    solution of Java Username Validation

---
+++ src/Introduction/JavaUsernameValidation.java
@@ -0,0 +1,27 @@
+package Introduction;
+
+import java.util.Scanner;
+class UsernameValidator {
+    /*
+     * Write regular expression here.
+     */
+    public static final String regularExpression = "^[a-zA-Z][a-zA-Z_0-9]{7,29}";
+}
+
+
+public class JavaUsernameValidation {
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }
+        }
+    }
+}
