6582a3ab2696cb1e46540c67595f37c3dd8b78dc
justignatoff <justignatoff@gmail.com>
2019-03-25 01:28:04 +0300

    solution of Circular Array Rotation

---
+++ src/algorithms/Implementation/CircularArrayRotation.java
@@ -0,0 +1,67 @@
+package algorithms.Implementation;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CircularArrayRotation {
+    // Complete the circularArrayRotation function below.
+    static int[] circularArrayRotation(int[] a, int k, int[] queries) {
+        int[] res = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int pos = queries[i] - (k % a.length);
+            pos = pos >= 0 ? pos : pos + a.length;
+            res[i] = a[pos];
+        }
+        return res;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nkq = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nkq[0]);
+
+        int k = Integer.parseInt(nkq[1]);
+
+        int q = Integer.parseInt(nkq[2]);
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int[] queries = new int[q];
+
+        for (int i = 0; i < q; i++) {
+            int queriesItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            queries[i] = queriesItem;
+        }
+
+        int[] result = circularArrayRotation(a, k, queries);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
