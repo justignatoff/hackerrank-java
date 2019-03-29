d09f6eb2846be4ad142dc45dcb762af581ed80e6
justignatoff <justignatoff@gmail.com>
2019-03-30 00:09:31 +0300

    solution of Extra Long Factorials

---
+++ src/algorithms/Implementation/ExtraLongFactorials.java
@@ -0,0 +1,26 @@
+package algorithms.Implementation;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class ExtraLongFactorials {
+    // Complete the extraLongFactorials function below.
+    static void extraLongFactorials(int n) {
+        BigInteger factorial = BigInteger.valueOf(1);
+        for (int i = 1; i <= n; i++) {
+            factorial = factorial.multiply(BigInteger.valueOf(i));
+        }
+        System.out.println(factorial);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        extraLongFactorials(n);
+
+        scanner.close();
+    }
+}
