99db97090c0457e26b7cfe1fe5f1096a29eb8aeb
justignatoff <justignatoff@gmail.com>
2019-04-01 08:50:55 +0300

    solution of Sherlock and Squares

---
+++ src/algorithms/Implementation/SherlockAndSquares.java
@@ -0,0 +1,41 @@
+package algorithms.Implementation;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class SherlockAndSquares {
+    static int squares(int a, int b) {
+        final int[] squares = IntStream.range(1, 31624).map(i -> i * i).toArray();
+        return (int) Arrays.stream(squares).filter(i -> i >= a && i <= b).count();
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String[] ab = scanner.nextLine().split(" ");
+
+            int a = Integer.parseInt(ab[0]);
+
+            int b = Integer.parseInt(ab[1]);
+
+            int result = squares(a, b);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
