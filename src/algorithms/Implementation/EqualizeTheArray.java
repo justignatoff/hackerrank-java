503a9f348a63881c87e77faa84f6655de945917a
justignatoff <justignatoff@gmail.com>
2019-04-06 06:33:37 +0300

    solution of Equalize the Array

---
+++ src/algorithms/Implementation/EqualizeTheArray.java
@@ -0,0 +1,48 @@
+package algorithms.Implementation;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class EqualizeTheArray {
+    // Complete the equalizeArray function below.
+    static int equalizeArray(int[] arr) {
+        Map<Integer, Integer> numCounts= new HashMap<>();
+        for(int el : arr) {
+            numCounts.put(el, numCounts.getOrDefault(el, 0) + 1);
+        }
+        return arr.length - numCounts.values().stream()
+                .max(Integer::compareTo).get();
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = equalizeArray(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
