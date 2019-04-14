4d141ae4f3b83ca6620d4547377037aed38addc4
justignatoff <justignatoff@gmail.com>
2019-04-14 21:47:11 +0300

    solution of Service Lane

---
+++ src/algorithms/Implementation/ServiceLane.java
@@ -0,0 +1,70 @@
+package algorithms.Implementation;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ServiceLane {
+    // Complete the serviceLane function below.
+    static int[] serviceLane(int n, int[][] cases, List<Integer> width) {
+        int[] res = new int[cases.length];
+        for (int i = 0; i < cases.length ; i++) {
+            res[i] = width.subList(cases[i][0], cases[i][1] + 1)
+                    .stream().min(Integer::compareTo).get();
+        }
+        return res;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nt = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nt[0]);
+
+        int t = Integer.parseInt(nt[1]);
+
+        List<Integer> width = new ArrayList<>();
+
+        String[] widthItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int widthItem = Integer.parseInt(widthItems[i]);
+            width.add(widthItem);
+        }
+
+        int[][] cases = new int[t][2];
+
+        for (int i = 0; i < t; i++) {
+            String[] casesRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 2; j++) {
+                int casesItem = Integer.parseInt(casesRowItems[j]);
+                cases[i][j] = casesItem;
+            }
+        }
+
+        int[] result = serviceLane(n, cases, width);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
