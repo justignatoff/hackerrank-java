ca1036f2413b293eefb2f289c94437d8917a6e81
justignatoff <justignatoff@gmail.com>
2019-05-16 00:20:40 +0300

    solution of The Love-Letter Mystery

---
+++ src/algorithms/Implementation/TheLoveLetterMystery.java
@@ -0,0 +1,38 @@
+package algorithms.Implementation;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class TheLoveLetterMystery {
+    // Complete the theLoveLetterMystery function below.
+    static int theLoveLetterMystery(String s) {
+        int res = IntStream.range(0, s.length() / 2)
+                .map(i -> Math.abs(s.charAt(i) - s.charAt(s.length() - i - 1))).sum();
+        return res;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            int result = theLoveLetterMystery(s);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
