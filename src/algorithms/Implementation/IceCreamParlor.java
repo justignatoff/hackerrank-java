77b3a09ecf6baaf2de1cf94561a28da43c975fdd
justignatoff <justignatoff@gmail.com>
2019-05-24 03:26:06 +0300

    solution of Ice Cream Parlor

--- src/algorithms/Implementation/IceCreamParlor.java
+++ src/algorithms/Implementation/IceCreamParlor.java
@@ -10,9 +10,7 @@ public class IceCreamParlor {
     static int[] icecreamParlor(int m, int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] + arr[j] == m) {
-                    return new int[]{i + 1, j + 1};
-                }
+                if (arr[i] + arr[j] == m) return new int[]{i + 1, j + 1};
             }
         }
         return new int[2];
