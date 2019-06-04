ab51c185f3a72b01f868e3bd28f2059e48ff0e9f
justignatoff <justignatoff@gmail.com>
2019-06-05 00:26:59 +0300

    solution of Permuting Two Arrays

---
+++ src/algorithms/Implementation/PermutingTwoArrays.java
@@ -0,0 +1,67 @@
+package algorithms.Implementation;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class PermutingTwoArrays {
+    // Complete the twoArrays function below.
+    static String twoArrays(int k, int[] A, int[] B) {
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        return IntStream
+                .range(0, A.length)
+                .map(i -> A[i] + B[B.length - i - 1])
+                .filter(i -> i >= k).count() == A.length ? "YES" : "NO";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String[] nk = scanner.nextLine().split(" ");
+
+            int n = Integer.parseInt(nk[0]);
+
+            int k = Integer.parseInt(nk[1]);
+
+            int[] A = new int[n];
+
+            String[] AItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int AItem = Integer.parseInt(AItems[i]);
+                A[i] = AItem;
+            }
+
+            int[] B = new int[n];
+
+            String[] BItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int BItem = Integer.parseInt(BItems[i]);
+                B[i] = BItem;
+            }
+
+            String result = twoArrays(k, A, B);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
