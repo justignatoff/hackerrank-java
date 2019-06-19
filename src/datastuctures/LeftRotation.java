37637967f092f636d9da4fc6bdcdf15c268d3a5b
justignatoff <justignatoff@gmail.com>
2019-06-20 01:18:34 +0300

    solution of Left Rotation

---
+++ src/datastuctures/LeftRotation.java
@@ -0,0 +1,41 @@
+package datastuctures;
+
+import java.util.Scanner;
+
+public class LeftRotation {
+    private static int[] leftRotation(int[] a, int d) {
+        int[] res = new int[a.length];
+        for (int i = 0, length = a.length; i < length; i++) {
+            res[i] = a[(i + d) % length];
+        }
+        return res;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        a = leftRotation(a, d);
+
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
+
+        scanner.close();
+    }
+}
